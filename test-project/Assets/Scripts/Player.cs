ee141296a668d97808b4e432c7cac92b99ebc7cb
pmsmith0491 <60986091+pmsmith0491@users.noreply.github.com>
2022-01-25 23:22:09 -0500

    Changed Camera Angle
    Changed the camera angle to look over the player.

---
+++ test-project/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Transform groundCheckTransform = null;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody rigidBodyComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Check if space key is pressed down
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+
+    // FixedUpdate is called once every physic update
+    private void FixedUpdate()
+    {
+
+        if(Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length == 1)
+        {
+            // ASSERT: No overlap with an object, we are in the air. 
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, verticalInput);
+
+    }
+
+    
+
+}
