ddbfd76741f0908ca56106d3bbb86f4f3272bdaf
pmsmith0491 <60986091+pmsmith0491@users.noreply.github.com>
2022-01-27 16:11:55 -0500

    Updated Level
    Level is changed. Prefabs have been altered.

--- test-project/Assets/Scripts/CameraFollow.cs
+++ test-project/Assets/Scripts/CameraFollow.cs
@@ -6,15 +6,25 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform target;
-
+    public bool smoothCameraMovement;
     public float smoothSpeed = 0.125f;
     public Vector3 offset; // offset of the camera (this is determined manually in the Unity Editor)
 
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // Lerp stands for linear interpolation 
-        transform.position = desiredPosition;
+
+        Vector3 desiredPosition = target.position + offset; // desired camera position relative to the player 
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // smoothed/linearly interpolated version of desiredPosition 
+                                                                                                   // Lerp stands for linear interpolation 
+        Vector3 newPosition;
+        if(smoothCameraMovement)
+        {
+            newPosition = smoothedPosition;
+        } else
+        {
+            newPosition = desiredPosition;
+        }
+        transform.position = newPosition;
         transform.LookAt(target);
     }
 }
