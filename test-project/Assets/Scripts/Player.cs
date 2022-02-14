e8dd18b4f623d18052e882c11512d88835182c4f
pmsmith0491 <60986091+pmsmith0491@users.noreply.github.com>
2022-02-14 15:55:45 -0500

    added new player model., idle, and walk animation

--- test-project/Assets/Scripts/Player.cs
+++ test-project/Assets/Scripts/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask playerMask;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float speed;
+
+    //Animator 
+    private Animator animator;
+
     private Rigidbody rigidBodyComponent;
     private float horizontalInput;
     private float verticalInput;
@@ -17,6 +21,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidBodyComponent = GetComponent<Rigidbody>();
     }
 
@@ -31,16 +36,17 @@ public class Player : MonoBehaviour
     // FixedUpdate is called once every physic update
     private void FixedUpdate()
     {
-
         rigidBodyComponent.velocity = new Vector3(horizontalInput, 0, verticalInput);
         rigidBodyComponent.velocity.Normalize();
         transform.Translate((rigidBodyComponent.velocity) * speed * Time.deltaTime, Space.World);
       
         if(rigidBodyComponent.velocity != Vector3.zero)
         {
-            Quaternion toRotation = Quaternion.LookRotation((-1) * rigidBodyComponent.velocity, Vector3.up);
+            Quaternion toRotation = Quaternion.LookRotation(rigidBodyComponent.velocity, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+
+        animator.SetFloat("speed", rigidBodyComponent.velocity.magnitude);
 
     }    
 
