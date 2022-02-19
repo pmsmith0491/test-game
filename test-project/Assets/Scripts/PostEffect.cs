5cba5efa3f0a7ffd49f1b6e2cc808fe499146f3a
pmsmith0491 <60986091+pmsmith0491@users.noreply.github.com>
2022-02-18 19:23:08 -0500

    started work on shaders

---
+++ test-project/Assets/Scripts/PostEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect : MonoBehaviour
+{
+
+    public Material mat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // source is the full rendered scene that you would normally
+        // send directly to the monitor. We are intercepting this so we can
+        // do a bit more work, before passing it on. 
+
+        Graphics.Blit(source, destination, mat);
+    }
+}
